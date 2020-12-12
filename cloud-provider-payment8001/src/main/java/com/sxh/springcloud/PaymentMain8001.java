787293dc83db466a29bfcccff0a6486a94596b89
sxh <xuhang_shang@163.com>
2020-12-13 01:37:39 +0800

    【Eureka】引入org.springframework.cloud.client.discovery.DiscoveryClient，获取注册中心中微服务的信息

--- cloud-provider-payment8001/src/main/java/com/sxh/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/sxh/springcloud/PaymentMain8001.java
@@ -8,6 +8,7 @@ package com.sxh.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @MapperScan("com.sxh.springcloud.dao")
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
--- cloud-provider-payment8001/src/main/java/com/sxh/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/sxh/springcloud/controller/PaymentController.java
@@ -10,9 +10,12 @@ import com.sxh.springcloud.entities.Payment;
 import com.sxh.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author SXH
@@ -27,6 +30,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -48,5 +54,18 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "no record, search id:" + id + " serverPort:" + serverPort, null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object getDiscoveryClient() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
