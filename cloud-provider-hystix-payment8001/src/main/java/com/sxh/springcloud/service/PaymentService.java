c76d926786c358bef3807f4dc0b3ec0d7829a9c9
sxh <xuhang_shang@163.com>
2020-12-16 00:01:10 +0800

    【Hystrix】使用@HystrixCommand、@EnableCircuitBreaker 等注解为服务提供端添加降级配置

--- cloud-provider-hystix-payment8001/src/main/java/com/sxh/springcloud/service/PaymentService.java
+++ cloud-provider-hystix-payment8001/src/main/java/com/sxh/springcloud/service/PaymentService.java
@@ -5,6 +5,8 @@
 
 package com.sxh.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -16,19 +18,36 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class PaymentService {
-    // 成功
+    /**
+     * 成功调用
+     */
     public String paymentInfo_OK(Integer id) {
-        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  "
+                + id + "\t" + "O(∩_∩)O哈哈~";
     }
 
-    // 失败
+    /**
+     * 失败，测试服务降级
+     */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOut_Handler", commandProperties =
+            {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+                    value = "3000")}) // 超过3 秒，即执行降级措施
     public String paymentInfo_TimeOut(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 5;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id:"
+                + id + "\t" + "呜呜 ┭┮﹏┭┮" + " 耗时(秒)" + timeNumber;
+    }
+
+    /**
+     * 服务调用失败的降级措施
+     */
+    public String paymentInfo_TimeOut_Handler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "   系统繁忙, 请稍候再试  ,id:"
+                + id + "\t" + "o(╥﹏╥)o";
     }
 }
